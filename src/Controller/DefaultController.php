b0e43fdabe60e2c4b20b939911de9eae10293858
Tom Olson <olson@webtoaster.com>
2019-07-10 11:07:37 -0500

    Cleaned up Default Controller and moved test code to a different controller annotated in GitIgnore

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 	
 	namespace App\Controller;
 	
-	use App\Repository\PersonRepository;
 	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 	use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Routing\Annotation\Route;
@@ -20,21 +19,6 @@
 		 */
 		public function index()
 		{
-			return $this->render('default/index.html.twig', [
-				'controller_name' => 'DefaultController',
-			]);
-		}
-		
-		/**
-		 * @Route("/test", name="app_test")
-		 * @param   PersonRepository   $personRepository
-		 *
-		 * @return Response
-		 */
-		public function test(PersonRepository $personRepository)
-		{
-			$personRepository->delete_orphaned_registrants();
-			
 			return $this->render('default/index.html.twig', [
 				'controller_name' => 'DefaultController',
 			]);
